ce8fe213a56fea47c3adb444b0133682f8c0db41
bengaudry <bengaudry@outlook.fr>
2026-05-29 12:08:50 +0200

    db: migration table musique

--- database/migrations/2026_05_29_100610_create_album_table.php
+++ database/migrations/2026_05_29_100610_create_album_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('musique', function (Blueprint $table) {
+        Schema::create('album', function (Blueprint $table) {
             $table->id();
-            $table->string("nom");
-            $table->integer("dureeSecondes");
-            $table->decimal("prix", 6, 2);
+            $table->timestamps();
         });
     }
 
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('musique');
+        Schema::dropIfExists('album');
     }
 };
