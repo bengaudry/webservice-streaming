32d8609befba947c7fa84eda047c0c4083833edc
bengaudry <bengaudry@outlook.fr>
2026-05-29 12:33:44 +0200

    db: migration table styles associes musique

---
+++ database/migrations/2026_05_29_103034_create_styles_associes_musique_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('styles_associes_musique', function (Blueprint $table) {
+            $table->foreignId("musique_id")->constrained("Musique")->cascadeOnDelete();
+            $table->foreignId("style_id")->constrained("Style")->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('styles_associes_musique');
+    }
+};
