3a217836b5f17b17e252ae5dea0b3d913a1f0576
bengaudry <bengaudry@outlook.fr>
2026-05-29 12:25:02 +0200

    db: migration table playlists

--- database/migrations/2026_05_29_102254_create_playlists_table.php
+++ database/migrations/2026_05_29_102254_create_playlists_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string("nom");
-            $table->foreignId("utilisateur_id")->constrained();
+            $table->foreignId("user_id")->constrained();
         });
     }
 
